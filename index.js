8e8d76b359f75f08ab2585f8abae2feb2d8cd48a
Josh Wright <joshwrightaudio@gmail.com>
2021-04-19 08:27:34 -0400

    node modules added...gitignore file created

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const inquirer = require("inquirer");
 const connection = mysql.createConnection({
   host: "localhost",
 
-
   port: 3306,
   user: "root",
   password: "IT2me@2432",
@@ -16,3 +15,25 @@ connection.connect((err) => {
   console.log(`connected as id ${connection.threadId}`);
   connection.end();
 });
+
+const start = () => {
+  inquirer.prompt({
+    name: "mainMenu",
+    type: "list",
+    message: "WHAT WOULD YOU LIKE TO DO?",
+    choices: [
+      "VIEW ALL EMPLOYEES",
+      "VIEW EMPLOYEES BY DEPARTMENT",
+      "VIEW EMPLOYEES BY ROLE",
+      "ADD EMPLOYEE",
+      "REMOVE EMPLOYEE",
+      "ADD ROLE",
+      "DELETE ROLE",
+      "ADD DEPARTMENT",
+      "DELETE DEPARTMENT",
+      "UPDATE EMPLOYEE ROLE",
+      "UPDATE EMPLOYEE DEPARTMENT",
+      "UPDATE EMPLOYEE MANAGER"
+    ],
+  });
+};
