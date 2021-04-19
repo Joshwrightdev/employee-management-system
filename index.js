7116c506b637e552b244fd34975405702d52a2a6
Josh Wright <joshwrightaudio@gmail.com>
2021-04-19 08:11:57 -0400

    added dependencies//js

---
+++ index.js
@@ -0,0 +1,18 @@
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+
+
+  port: 3306,
+  user: "root",
+  password: "IT2me@2432",
+  database: "employee_db",
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log(`connected as id ${connection.threadId}`);
+  connection.end();
+});
